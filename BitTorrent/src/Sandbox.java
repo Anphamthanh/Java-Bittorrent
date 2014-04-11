9c90ae74972973075c3be493ba12072a38b4f60f
PhucNoel <phucnvo@gmail.com>
2014-04-11 01:27:41 -0400

    asd

--- BitTorrent/src/Sandbox.java
+++ BitTorrent/src/Sandbox.java
@@ -1,20 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream.GetField;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
-
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.URL;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Sandbox {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnknownHostException,
+			SocketException {
 		// TODO Auto-generated method stub
-//		char high = 0xc8;
-//		char low = 0xd5;
-//		
-//		System.out.print(bytesToUnsignedDec(high, low));
+//		Enumeration e = NetworkInterface.getNetworkInterfaces();
+//		while (e.hasMoreElements()) {
+//			NetworkInterface n = (NetworkInterface) e.nextElement();
+//			Enumeration ee = n.getInetAddresses();
+//
+//			while (ee.hasMoreElements()) {
+//				InetAddress i = (InetAddress) ee.nextElement();
+//				String ip = i.getHostAddress();
+//
+//				if (validate(ip)) {
+//					System.out.println(ip);
+//				}
+//
+//			}
+//		}
 	}
-	
-	private static int bytesToUnsignedDec(char high, char low){
-		int retVal = (int)((high<<8) | low);
-		return retVal;
-	}
+
+
 
 }
